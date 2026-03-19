ef5068e9213ca7b8e350bb75eb01b12bd05c5a29
lbwright22ci <lbwright22@gmail.com>
2026-03-19 13:08:13 +0000

    add meta data and method to Post and Comment models

--- blog/models.py
+++ blog/models.py
@@ -18,6 +18,10 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering =["-created_on", "author"]
+    def __str__(self):
+        return f"{self.title} | written by {self.author}"
 
 class Comment(models.Model):
     Post = models.ForeignKey(
@@ -29,3 +33,7 @@ class Comment(models.Model):
     created_on = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     approved=models.BooleanField(default=False)
+    class Meta:
+        ordering =["created_on"]
+    def __str__(self):
+        return f"Comment {self.body} by {self.author}"
